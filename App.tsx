562c2a31f1fc22fe32bb87a1ac5fbd42472de93b
Mark Lawlor <mwlawlor@gmail.com>
2022-11-03 13:07:00 +1000

    Jonatthu button

--- App.tsx
+++ App.tsx
@@ -1,19 +1,36 @@
-import {useColorScheme} from 'nativewind';
+import {useColorScheme, styled} from 'nativewind';
 import React from 'react';
-import {Text, Pressable} from 'react-native';
+import {View, Text, Pressable} from 'react-native';
 
 import './styles/global.css';
+
+const Button = styled(Pressable, 'border', {
+  variants: {
+    status: {
+      primary: 'border-red-400',
+      secondary: 'border-blue-500',
+    },
+  },
+  defaultVariants: {
+    status: 'primary',
+  },
+});
 
 const App = () => {
   const {colorScheme, toggleColorScheme} = useColorScheme();
   return (
-    <Pressable
-      className="flex-1 items-center justify-center bg-white dark:bg-black"
-      onPress={() => toggleColorScheme()}>
-      <Text className="custom-color dark:text-white">
-        {`Open up App.js to start working on your app! ${colorScheme} `}
-      </Text>
-    </Pressable>
+    <View className="flex-1 items-center justify-center bg-white dark:bg-black">
+      <Button
+        onPress={() => toggleColorScheme()}
+        className="flex-1 bg-slate-300 dark:bg-slate-800">
+        <Text className="brand-color dark:text-white">
+          Open up App.js to start working on your app!
+        </Text>
+        <Text className="brand-color dark:text-white">
+          {colorScheme === 'dark' ? '🌙 Dark' : '🌞 Light'}
+        </Text>
+      </Button>
+    </View>
   );
 };
 
